82db5760098a36c0cd62d30d1b3551312864b064
Sonali Kumari <kumarisona0203@gmail.com>
2026-01-30 12:39:20 +0530

    commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const menuIcon = document.querySelector('#menu-icon');
+ const navbar = document.querySelector('.navbar');
+
+
+ menuIcon.onclick = () => {
+   menuIcon.classList.toggle('fa-bars');
+    menuIcon.classList.toggle('fa-x');
+    navbar.classList.toggle('active');
+ }
+
+ 
